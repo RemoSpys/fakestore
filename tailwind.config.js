beb51cd64093f72e4cf4b1c1b79988b840bd1f1b
RemoSpys <104862738+RemoSpys@users.noreply.github.com>
2024-09-27 12:10:34 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}", // Matches all .vue, .js, .ts, .jsx, and .tsx files in the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
